2727ef9df290edc47bbad3072a2701e248256a63
Sammed S P <127593704+psammy171@users.noreply.github.com>
2024-05-20 19:40:00 +0530

    update color scheme and logo

--- app/profile/header.tsx
+++ app/profile/header.tsx
@@ -4,7 +4,7 @@ import Profile from "@/components/header/profile";
 const Header = () => {
   return (
     <div className="w-full h-12 bg-primary-700 text-white flex items-center px-2 absolute top-0">
-      <Logo />
+      <Logo className="w-7 h-7" />
       <span className="flex-grow"></span>
       <Profile />
     </div>
