41aba42ae6e4ea555eb82065df3898d00df104c9
Sammed S P <127593704+psammy171@users.noreply.github.com>
2024-05-21 15:05:50 +0530

    add disconnect feature

--- app/profile/wallet/ConnectWallet.tsx
+++ app/profile/wallet/ConnectWallet.tsx
@@ -1,9 +1,15 @@
 import Button from "@/components/ui/button";
-import { Connector, useAccount, useConnect } from "wagmi";
+import Loader from "@/components/ui/loader";
+import { useEffect, useState } from "react";
+import { Connector, useAccount, useConnect, useDisconnect } from "wagmi";
+import { getBalance } from "wagmi/actions";
+import { config } from "./Wallet";
 
 const ConnectWallet = () => {
-  const { isConnected, address } = useAccount();
+  const { isConnected, address, isConnecting } = useAccount();
   const { connectors, connect } = useConnect();
+  const { disconnect } = useDisconnect();
+  const [ethBal, setEthBal] = useState<string>("");
   const metaMaskConnector: Connector | undefined = connectors.find(
     (connector) => connector.name === "MetaMask"
   );
@@ -13,17 +19,52 @@ const ConnectWallet = () => {
     }
   };
 
+  useEffect(() => {
+    const fetchBalance = async () => {
+      const balance = await getBalance(config, {
+        address: address!,
+      });
+      setEthBal(balance.formatted.toString());
+    };
+    if (isConnected && address) {
+      fetchBalance();
+    }
+  }, [address, isConnected]);
+
   return (
-    <div className="flex flex-col items-center mt-32">
-      {isConnected ? (
-        <div>
-          <p>Wallet connected</p>
-          <p>Address : {address}</p>
+    <>
+      {isConnecting ? (
+        <div className="flex flex-col items-center mt-32">
+          <Loader size="large" className="border-[4px]" />
+          <p>Connecting to wallet</p>
         </div>
       ) : (
-        <Button onClick={connectHandler}>Connect Metamask wallet</Button>
+        <>
+          <div className="flex flex-col items-center mt-32">
+            {isConnected ? (
+              <div>
+                <p>Wallet connected</p>
+                <p>Address : {address}</p>
+                <p>Balance : {ethBal}</p>
+              </div>
+            ) : (
+              <Button onClick={connectHandler}>Connect Metamask wallet</Button>
+            )}
+          </div>
+          {isConnected && (
+            <Button
+              className="absolute bottom-5 right-5 min-w-44"
+              variant="error"
+              onClick={() => {
+                disconnect();
+              }}
+            >
+              Disconnect Wallet
+            </Button>
+          )}
+        </>
       )}
-    </div>
+    </>
   );
 };
 
--- app/profile/wallet/Wallet.tsx
+++ app/profile/wallet/Wallet.tsx
@@ -3,24 +3,24 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
 import { WagmiProvider, createConfig, http } from "wagmi";
-import { polygonMumbai } from "wagmi/chains";
+import { mainnet } from "wagmi/chains";
 import { metaMask } from "wagmi/connectors";
 import ConnectWallet from "./ConnectWallet";
 import DetectWallet from "./DetectWallet";
 import InstallWallet from "./InstallMetaMask";
 
+export const config = createConfig({
+  chains: [mainnet],
+  connectors: [metaMask()],
+  transports: {
+    [mainnet.id]: http(),
+  },
+});
+
 const Wallet = () => {
   const queryClient = new QueryClient();
   const [detectingWallet, setDetectingWallet] = useState<boolean>(true);
   const [isWalletInstalled, setIsWalletInstalled] = useState<boolean>(false);
-
-  const config = createConfig({
-    chains: [polygonMumbai],
-    connectors: [metaMask()],
-    transports: {
-      [polygonMumbai.id]: http(),
-    },
-  });
 
   const setWalletInstalledFalg = (isInstalled: boolean) => {
     setIsWalletInstalled(isInstalled);
