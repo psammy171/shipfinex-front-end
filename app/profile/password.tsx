27a9fadff72160a389364831f5eef6334f780c97
Sammed S P <127593704+psammy171@users.noreply.github.com>
2024-05-23 12:36:25 +0530

    add password change fields

--- app/profile/password.tsx
+++ app/profile/password.tsx
@@ -1,7 +1,18 @@
+import Button from "@/components/ui/button";
+import Input from "@/components/ui/input";
+import Label from "@/components/ui/label";
+
 const Password = () => {
   return (
-    <div>
-      <p>Password component</p>
+    <div className="py-10">
+      <p className="text-center">Change Password</p>
+      <form className="flex flex-col max-w-[400px] mx-auto">
+        <Label inputLabel="New Password" />
+        <Input placeholder="Enter new password" type="password" />
+        <Label inputLabel="Confirm Password" />
+        <Input placeholder="Confirm password" type="password" />
+        <Button className="mx-0 mt-4">Change Password</Button>
+      </form>
     </div>
   );
 };
